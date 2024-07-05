febf6df852333f0c387f750314f898bd2b2e8f41
charmainelow1 <125853983+charmainelow1@users.noreply.github.com>
2024-07-05 18:21:43 +0800

    4.2

--- models/blog.js
+++ models/blog.js
@@ -1,8 +1,6 @@
 const config = require("../utils/config")
 const mongoose = require('mongoose')
 
-mongoose.connect(config.MONGODB_URL)
-
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
