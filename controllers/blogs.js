11d93367ed86f6e699ee91e2940e31c76cb6d67d
charmainelow1 <125853983+charmainelow1@users.noreply.github.com>
2024-07-08 13:42:51 +0800

    4.13-4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  
+
   response.json(blogs)
 })
 
@@ -12,6 +12,22 @@ blogsRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id, 
+    { title, author, url, likes }, 
+    { new: true, context: 'query' }
+  )
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const { title } = require('node:process')
 
 const api = supertest(app)
 
@@ -59,53 +60,95 @@ const initialBlogs = [
 ]
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
+  await Blog.deleteMany({})
 
-    const blogObjects = initialBlogs.map(blog => new Blog(blog))
-    const promiseArray = blogObjects.map(blog => blog.save())
-    await Promise.all(promiseArray)
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
 test('blogs are returned as json', async () => {
-    await api 
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  await api 
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
 })
 
 test('returned correct number of blogs', async () => {
-    const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs')
 
-    assert.strictEqual(response.body.length, initialBlogs.length)
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 test('unique identifier property of blogs posts is id', async () => {
-    const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs')
 
-    assert(response.body[0].hasOwnProperty("id"))
+  assert(response.body[0].hasOwnProperty("id"))
 })
 
 test('http post request creates new blog post', async () => {
-    const newBlog = {
-        title: "This is a new post",
-        author: "Tom Jerry",
-        url: "https://newpost.com",
-        likes: 1
-    }
+  const newBlog = {
+    title: "This is a new post",
+    author: "Tom Jerry",
+    url: "https://newpost.com",
+    likes: 1
+  }
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
 
-    const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs')
 
-    const titles = response.body.map(r => r.title)
+  const titles = response.body.map(r => r.title)
 
-    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
 
-    assert(titles.includes('This is a new post'))
+  assert(titles.includes('This is a new post'))
+})
+
+test('http delete request deletes blog post', async () => {
+  const startResponse = await api.get('/api/blogs')
+  const blogsAtStart = startResponse.body
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const endResponse = await api.get('/api/blogs')
+  const blogsAtEnd = endResponse.body 
+  
+  const titles = blogsAtEnd.map(r => r.title)
+
+  assert(!titles.includes(blogToDelete.content))
+
+  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
+})
+
+test.only('http put request updates blog post', async () => {
+  const startResponse = await api.get('/api/blogs')
+  const blogsAtStart = startResponse.body
+
+  const blogToUpdate = blogsAtStart[0]
+  const updatedInfo = {
+    "title": "React patterns",
+    "author": "Michael Chan",
+    "url": "thisisaurl",
+    "likes": 10
+  }
+
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedInfo)
+    .expect(200)
+
+  const endResponse = await api.get('/api/blogs')
+  const updatedBlogs = endResponse.body 
+
+  assert.strictEqual(updatedBlogs[0].likes, updatedInfo.likes)
 })
 
 after(async () => {
